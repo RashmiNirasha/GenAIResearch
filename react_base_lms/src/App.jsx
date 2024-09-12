041b1783862043c4312c13e5d1a69e806f6ee368
michellenikeetha <michellenikeetha@gmail.com>
2024-09-12 21:54:05 +0530

    Assignments and forgot password pages

--- react_base_lms/src/App.jsx
+++ react_base_lms/src/App.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
+import ForgotPassword from './components/ForgotPassword';
 import LandingPage from './components/LandingPage';
 import CoursesPage from './components/CoursesPage';
 import AnnouncementsPage from './components/AnnouncementsPage';
 import AnnouncementDetail from './components/AnnouncementDetail';
 import AssignmentsPage from './components/AssignmentsPage';
 import SubmittedAssignments from './components/SubmittedAssignments';
+import EditAssignment from './components/EditAssignment';
+import SubmitAssignment from './components/SubmitAssignment';
 import ProfilePage from './components/ProfilePage';
 
 function App() {
@@ -18,11 +21,14 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/courses" element={<CoursesPage />} />
         <Route path="/announcements" element={<AnnouncementsPage />} />
         <Route path="/announcements/ai-for-everyone" element={<AnnouncementDetail />} />
         <Route path="/assignments" element={<AssignmentsPage />} />
         <Route path="/submitted-assignments" element={<SubmittedAssignments />} />
+        <Route path="/submitted-assignments/edit-assignment" element={<EditAssignment />} />
+        <Route path="/assignments/submit-assignment" element={<SubmitAssignment />} />
         <Route path="/profile" element={<ProfilePage />} />
         {/* Add other routes here */}
       </Routes>
