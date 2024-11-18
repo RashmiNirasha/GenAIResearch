7f6c064fc503f56dc2400b180ca5520b2d22b7eb
michellenikeetha <michellenikeetha@gmail.com>
2024-11-18 22:54:59 +0530

    mycourse pages

--- react_base_lms/src/App.jsx
+++ react_base_lms/src/App.jsx
@@ -1,19 +1,20 @@
 // src/App.js
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from "./components/LandingPage";
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
 import ForgotPassword from './components/ForgotPassword';
-import LandingPage from './components/LandingPage';
 import CoursesPage from './components/CoursesPage';
-import AnnouncementsPage from './components/AnnouncementsPage';
-import AnnouncementDetail from './components/AnnouncementDetail';
+import ProfilePage from './components/ProfilePage';
 import AssignmentsPage from './components/AssignmentsPage';
 import SubmittedAssignments from './components/SubmittedAssignments';
 import EditAssignment from './components/EditAssignment';
 import SubmitAssignment from './components/SubmitAssignment';
-import ProfilePage from './components/ProfilePage';
+import AnnouncementsPage from './components/AnnouncementsPage';
+import AnnouncementDetail from './components/AnnouncementDetail';
 import MyLearning from './components/MyLearningPage';
+import MyCoursePage from './components/MyCoursePage.jsx';
 
 function App() {
   return (
@@ -24,14 +25,15 @@ function App() {
         <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/courses" element={<CoursesPage />} />
-        <Route path="/announcements" element={<AnnouncementsPage />} />
-        <Route path="/announcements/ai-for-everyone" element={<AnnouncementDetail />} />
+        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/assignments" element={<AssignmentsPage />} />
         <Route path="/submitted-assignments" element={<SubmittedAssignments />} />
         <Route path="/submitted-assignments/edit-assignment" element={<EditAssignment />} />
         <Route path="/assignments/submit-assignment" element={<SubmitAssignment />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/announcements" element={<AnnouncementsPage />} />
+        <Route path="/announcements/ai-for-everyone" element={<AnnouncementDetail />} />
         <Route path="/my-learning" element={<MyLearning />} />
+        <Route path="/my-learning/my-course" element={<MyCoursePage />} />
       </Routes>
     </Router>
   );
